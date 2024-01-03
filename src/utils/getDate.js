c16bafc96cd175dbd1d00798466479893b2df713
Nursat Bakyt <itsnoorsat@MacBook-Air-Nursat.local>
2024-01-03 15:17:16 +0600

    https

--- src/utils/getDate.js
+++ src/utils/getDate.js
@@ -1,6 +1,6 @@
 import moment from "moment";
 
-const response = await fetch('http://worldtimeapi.org/api/ip');
+const response = await fetch('https://worldtimeapi.org/api/ip');
 const data = await response.json();
 
 const date = moment(data.datetime).format("YYYY-MM-DDTHH:mm:ss")
